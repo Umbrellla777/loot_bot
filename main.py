8ab1fc8d94e2567f65fdf6eed489c73d8611cbd0
Andrew <161312334+Umbrellla777@users.noreply.github.com>
2024-08-06 12:05:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+####--------------------------------####
+#--# Author:   by Umbrellla777      #--#
+#--# Telegram: @Umbrellla777        #--#
+#--# VK:       @Umbrellla777        #--#
+####--------------------------------####
+
+###########################
+## Импорт библиотек
+###########################
+import sys
+import asyncio
+import time
+import emoji
+import random
+
+
+from asyncio import sleep
+from collections import deque
+from telethon.sync import TelegramClient
+from telethon import TelegramClient
+from telethon import events
+
+##from telethon                       import functions, types
+##from telethon.tl.types              import ChatBannedRights
+##from telethon.tl.functions.users    import GetFullUserRequest
+##from telethon.tl.functions.channels import EditBannedRequest
+
+
+###########################
+## Цвет консоли
+###########################
+red = [206, 76, 54]
+green = [68, 250, 123]
+blue = [253, 127, 233]
+yellow = [241, 250, 118]
+orange = [255, 184, 107]
+
+
+def colored(color, text):
+    return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(color[0], color[1], color[2], text)
+
+
+###########################
+## Настройки
+###########################
+api_id = int(sys.argv[1])
+api_hash = str(sys.argv[2])
+
+## Connect
+client = TelegramClient('users/current_user', api_id, api_hash)
+client.start()
+
+###########################
+## Импорт папок|добавить
+###########################
+
+###########################
+## Информация о аккаунте
+###########################
+entity = client.get_entity("me")
+MY_ID = entity.id
+print(
+    "["
+    + colored(green, "PROFILE: ")
+    + str(entity.first_name)
+    + " | " + colored(orange, "Id: ") + str(MY_ID)
+    + " | " + colored(orange, "Uname: ") + "@" + str(entity.username)
+    + "]"
+)
+
+###########################
+## Проверка бота
+###########################
+@client.on(events.NewMessage(pattern='.ping'))
+async def pong(event):
+    message = event.message
+    await message.edit("pong")
+###########################
+###########################
+client.on(events.NewMessage(chats="@testbotgiumb"))  
+async def handle_message(event):
+    message = event.message
+    sender = message.sender
+
+    
+    if ("@" in message.text) and ("Umbrellla777" in message.text.replace("@", "")) and (sender.id != MY_ID):
+        target_chat_id = @chaksads
+        await client.send_message(target_chat_id, "Привет, я выйграл") 
+
+
+client.run_until_disconnected()
